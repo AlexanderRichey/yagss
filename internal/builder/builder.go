63647b37f6d55ab3c782da2c0002afecb37f2207
Alexander Richey <alexander.richey@gmail.com>
2021-01-03 23:16:49 -0500

    pull out md handling

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -18,6 +18,7 @@ import (
 	"github.com/yuin/goldmark"
 	meta "github.com/yuin/goldmark-meta"
 	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 type Config struct {
@@ -31,6 +32,7 @@ type Config struct {
 	DefaultTitle        string
 	DefaultDescription  string
 	DefaultPostTemplate string
+	DefaultPageTemplate string
 }
 
 type Builder interface {
@@ -45,29 +47,17 @@ type builderImpl struct {
 }
 
 type postData struct {
-	markdownData
-
-	title       string
-	description string
-	date        time.Time
-}
-
-type markdownData struct {
-	markdown *bytes.Buffer
-	template *pongo2.Template
-}
-
-type publicAsset struct {
-	name         string
-	originalPath string
-	path         string
-	md5          string
+	Title       string
+	Description string
+	Date        time.Time
+	Markdown    string
 }
 
 var (
 	ErrNotDir                = errors.New("not a directory")
 	ErrNotSerializable       = errors.New("not serializable")
 	ErrRequriedFieldNotFound = errors.New("required field not found")
+	ErrInvalidFormat         = errors.New("invalid file format")
 )
 
 const (
@@ -90,7 +80,19 @@ func New(c *Config) (Builder, error) {
 		return nil, fmt.Errorf("could not load templates: %w", err)
 	}
 
-	builder.markdown = goldmark.New(goldmark.WithExtensions(meta.Meta))
+	err = pongo2.RegisterFilter("path", func(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+		m := in.Interface().(map[string]string)
+
+		return pongo2.AsValue(m[param.String()]), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not register filter: %w", err)
+	}
+
+	builder.markdown = goldmark.New(
+		goldmark.WithExtensions(meta.Meta),
+		goldmark.WithRendererOptions(html.WithUnsafe()),
+	)
 
 	return builder, nil
 }
@@ -149,8 +151,8 @@ func (b *builderImpl) Build() error {
 	return nil
 }
 
-func (b *builderImpl) handlePublic() ([]*publicAsset, error) {
-	publicAssets := make([]*publicAsset, 0)
+func (b *builderImpl) handlePublic() (map[string]string, error) {
+	publicAssets := make(map[string]string, 0)
 	hash := md5.New()
 
 	err := filepath.Walk(b.config.PublicDir, func(path string, info os.FileInfo, err error) error {
@@ -167,18 +169,7 @@ func (b *builderImpl) handlePublic() ([]*publicAsset, error) {
 				return nil
 			}
 
-			// We replace the first dir in the path with the output dir. We expect
-			// split[0] to be $b.config.PublicDir.
-			split := strings.Split(path, string(os.PathSeparator))
-			split[0] = b.config.OutputDir
-			dirP := filepath.Join(split...)
-
-			err = os.MkdirAll(dirP, os.FileMode(_ReadWriteExecute))
-			if err != nil {
-				return fmt.Errorf("could not create directory %q: %w", dirP, err)
-			}
-
-			return nil
+			return b.mkOutDir(path)
 		}
 
 		b.counter++
@@ -236,14 +227,10 @@ func (b *builderImpl) handlePublic() ([]*publicAsset, error) {
 			return fmt.Errorf("could not write file %q: %w", outP, err)
 		}
 
-		publicAssets = append(publicAssets, &publicAsset{
-			name: info.Name(),
-			// We slice off the first dir in the path because, when assets are
-			// served, they will be served from the root.
-			originalPath: filepath.Join(strings.Split(path, string(os.PathSeparator))[1:]...),
-			path:         filepath.Join(strings.Split(outP, string(os.PathSeparator))[1:]...),
-			md5:          hashS,
-		})
+		// We slice off the first dir in the path because it is redundant to include
+		// $b.config.PublicDir in every path
+		publicAssets[filepath.Join(strings.Split(path, string(os.PathSeparator))[1:]...)] =
+			"/" + strings.Join(strings.Split(outP, string(os.PathSeparator))[1:], "/")
 
 		fmt.Printf("DONE\n")
 
@@ -256,7 +243,7 @@ func (b *builderImpl) handlePublic() ([]*publicAsset, error) {
 	return publicAssets, nil
 }
 
-func (b *builderImpl) handlePosts(publicAssets []*publicAsset) ([]*postData, error) {
+func (b *builderImpl) handlePosts(publicAssets map[string]string) ([]*postData, error) {
 	postList := make([]*postData, 0)
 
 	// Create the output dir
@@ -286,76 +273,6 @@ func (b *builderImpl) handlePosts(publicAssets []*publicAsset) ([]*postData, err
 			return nil
 		}
 
-		fb, err := ioutil.ReadFile(path)
-		if err != nil {
-			return fmt.Errorf("could not read post %q: %w", path, err)
-		}
-
-		postD := new(postData)
-		postD.markdown = new(bytes.Buffer)
-
-		// Render markdown
-		ctx := parser.NewContext()
-		err = b.markdown.Convert(fb, postD.markdown, parser.WithContext(ctx))
-		if err != nil {
-			return fmt.Errorf("could not render markdown in %q: %w", path, err)
-		}
-
-		// Get front-matter
-		data, err := msi2mss(meta.Get(ctx))
-		if err != nil {
-			return fmt.Errorf("could not process front-matter on %q: %w", path, err)
-		}
-
-		// Check for required fields
-		for _, key := range []string{"title", "date"} {
-			if _, ok := data[key]; !ok {
-				return fmt.Errorf("%w: %q", ErrRequriedFieldNotFound, key)
-			}
-		}
-
-		// Gather metadata
-		postD.title = data["title"]
-
-		postD.date, err = time.Parse("2006-01-02", data["date"])
-		if err != nil {
-			return fmt.Errorf("could not parse date %q: %w", data["date"], err)
-		}
-
-		// Optional description metadata
-		if dat, ok := data["description"]; ok {
-			postD.description = dat
-		} else {
-			postD.description = b.config.DefaultDescription
-		}
-
-		// The default post template can be overridden with a front-matter
-		// directive
-		var tplP string
-		if dat, ok := data["template"]; ok {
-			tplP = dat
-		} else {
-			tplP = b.config.DefaultPostTemplate
-		}
-
-		// Get the base post template
-		postD.template, err = b.templates.FromFile(tplP)
-		if err != nil {
-			return fmt.Errorf("could not get template %q: %w", tplP, err)
-		}
-
-		// Compile an intermediate template in case there are template directives
-		// inside the markdown file
-		itpl, err := b.templates.FromString(postD.markdown.String())
-		if err != nil {
-			return fmt.Errorf("could not compile intermediate template: %w", err)
-		}
-
-		mdS, err := itpl.Execute(pongo2.Context{"assets": publicAssets})
-		if err != nil {
-			return fmt.Errorf("could not render intermediate template: %w", err)
-		}
-
 		// Determine the output path
 		split := strings.Split(path, string(os.PathSeparator))
 		split[0] = b.config.OutputDir
@@ -365,28 +282,44 @@ func (b *builderImpl) handlePosts(publicAssets []*publicAsset) ([]*postData, err
 		split[len(split)-1] = strings.Join(fsplit, ".")
 		outP := filepath.Join(split...)
 
-		// Finally create the output file and write content to it
-		outF, err := os.Create(outP)
-		if err != nil {
-			return fmt.Errorf("could not create file %q: %w", outP, err)
-		}
-		defer outF.Close()
+		pCtx, err := b.handleMd(path, outP, b.config.DefaultPostTemplate, publicAssets,
+			func(data map[string]string) (pongo2.Context, error) {
+				p2ctx := pongo2.Context{}
 
-		// We write the output from rendering the base template
-		err = postD.template.ExecuteWriter(pongo2.Context{
-			"content":     mdS,
-			"assets":      publicAssets,
-			"title":       b.config.DefaultTitle,
-			"postTitle":   postD.title,
-			"date":        postD.date,
-			"description": postD.description,
-			"extra":       data,
-		}, outF)
+				// Check for required fields
+				for _, key := range []string{"title", "date"} {
+					if _, ok := data[key]; !ok {
+						return nil, fmt.Errorf("%w: %q", ErrRequriedFieldNotFound, key)
+					}
+				}
+
+				// Gather metadata
+				p2ctx["title"] = data["title"]
+
+				p2ctx["date"], err = time.Parse("2006-01-02", data["date"])
+				if err != nil {
+					return nil, fmt.Errorf("could not parse date %q: %w", data["date"], err)
+				}
+
+				// Optional description metadata
+				if dat, ok := data["description"]; ok {
+					p2ctx["description"] = dat
+				} else {
+					p2ctx["description"] = b.config.DefaultDescription
+				}
+
+				return p2ctx, nil
+			})
 		if err != nil {
-			return fmt.Errorf("could not render template %q to %q: %w", tplP, outP, err)
+			return fmt.Errorf("error generating markdown: %w", err)
 		}
 
-		postList = append(postList, postD)
+		postList = append(postList, &postData{
+			Title:       pCtx["title"].(string),
+			Date:        pCtx["date"].(time.Time),
+			Description: pCtx["description"].(string),
+			Markdown:    pCtx["content"].(string),
+		})
 
 		fmt.Printf("DONE\n")
 
@@ -397,120 +330,198 @@ func (b *builderImpl) handlePosts(publicAssets []*publicAsset) ([]*postData, err
 	}
 
 	sort.SliceStable(postList, func(i, j int) bool {
-		return postList[i].date.Before(postList[j].date)
+		return postList[i].Date.Before(postList[j].Date)
 	})
 
 	return postList, nil
 }
 
-// func (b *builderImpl) handlePages(publicAssets []*publicAsset, postList []*markdownData) error {
-// 	return filepath.Walk(b.config.PagesDir, func(path string, info os.FileInfo, err error) error {
-// 		if err != nil {
-// 			return err
-// 		}
+func (b *builderImpl) handlePages(publicAssets map[string]string, postList []*postData) error {
+	return filepath.Walk(b.config.PagesDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-// 		if info.IsDir() {
-// 			if info.Name() != filepath.Base(b.config.PagesDir) {
-// 				dirPath := filepath.Join(b.config.OutputDir,
-// 					strings.TrimPrefix(path, b.config.PagesDir+string(filepath.Separator)))
-// 				err = os.MkdirAll(dirPath, os.FileMode(0755))
-// 				if err != nil {
-// 					return fmt.Errorf("could not create directory %q: %w", dirPath, err)
-// 				}
-// 			}
+		// Create a corresponding directory in the $b.config.OutputDir
+		if info.IsDir() {
+			// Do not create public directory inside the output dir itself.
+			// If we did not have this check, then a directory inside the output
+			// dir called $b.config.PublicDir would be created.
+			if info.Name() == filepath.Base(b.config.PagesDir) {
+				return nil
+			}
 
-// 			return nil
-// 		}
+			return b.mkOutDir(path)
+		}
 
-// 		b.counter++
-// 		fmt.Printf("==> Processing %q --> ", path)
+		b.counter++
+		fmt.Printf("==> Processing %q --> ", path)
 
-// 		switch filepath.Ext(path) {
-// 		case ".html":
-// 			err = b.handleHTMLFile(path)
-// 		case ".md":
-// 			// err = b.handleMarkdownFile(path)
-// 		default:
-// 			return fmt.Errorf("invalid file format: %s", path)
-// 		}
-// 		if err != nil {
-// 			return fmt.Errorf("error processing file %q: %w", path, err)
-// 		}
+		switch filepath.Ext(path) {
+		case ".html":
+			{
+				fb, err := ioutil.ReadFile(path)
+				if err != nil {
+					return fmt.Errorf("could not read file %q: %w", path, err)
+				}
 
-// 		fmt.Printf("DONE\n")
+				tpl, err := b.templates.FromBytes(fb)
+				if err != nil {
+					return fmt.Errorf("could not compile page %q: %w", path, err)
+				}
 
-// 		return nil
-// 	})
-// }
+				// Determine the output path
+				split := strings.Split(path, string(os.PathSeparator))
+				split[0] = b.config.OutputDir
+				outP := filepath.Join(split...)
 
-// func (b *builderImpl) handleMarkdownFile(path string) (*markdownData, error) {
-// 	fb, err := ioutil.ReadFile(path)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+				// Create the output file and write content to it
+				outF, err := os.Create(outP)
+				if err != nil {
+					return fmt.Errorf("could not create file %q: %w", outP, err)
+				}
+				defer outF.Close()
 
-// 	postData := new(markdownData)
+				err = tpl.ExecuteWriter(pongo2.Context{
+					"posts":  postList,
+					"assets": publicAssets,
+				}, outF)
+				if err != nil {
+					return fmt.Errorf("could not render page %q: %w", outP, err)
+				}
+			}
+		case ".md":
+			{
+				// Determine the output path
+				split := strings.Split(path, string(os.PathSeparator))
+				split[0] = b.config.OutputDir
+				fsplit := strings.Split(info.Name(), ".")
+				fsplit[len(fsplit)-1] = "html"
+				split[len(split)-1] = strings.Join(fsplit, ".")
+				outP := filepath.Join(split...)
 
-// 	ctx := parser.NewContext()
-// 	err = b.markdown.Convert(fb, postData.markdown, parser.WithContext(ctx))
-// 	if err != nil {
-// 		return nil, err
-// 	}
+				_, err = b.handleMd(path, outP, b.config.DefaultPageTemplate, publicAssets,
+					func(data map[string]string) (pongo2.Context, error) {
+						p2ctx := pongo2.Context{"posts": postList}
 
-// 	// md := meta.Get(ctx)
-// 	md := meta.GetItems(ctx)
-// 	data := make(map[string]string, 4)
-// 	for _, key := range []string{"Title", "Date", "Template", "Description"} {
-// 		si, ok := md[key]
-// 		if !ok {
-// 			return nil, fmt.Errorf("no %s directive in %q", key, path)
-// 		}
+						// Optional metadata
+						if dat, ok := data["description"]; ok {
+							p2ctx["description"] = dat
+						}
 
-// 		val, ok := si.(string)
-// 		if !ok {
-// 			return nil, fmt.Errorf("malformed %s directive in %q", key, path)
-// 		}
+						if dat, ok := data["title"]; ok {
+							p2ctx["title"] = dat
+						}
 
-// 		data[key] = val
-// 	}
+						return p2ctx, nil
+					})
+			}
+		default:
+			err = fmt.Errorf("%w: %q", ErrInvalidFormat, path)
+		}
+		if err != nil {
+			return fmt.Errorf("error processing file %q: %w", path, err)
+		}
 
-// 	// Gather metadata
-// 	postData.title = data["Title"]
-// 	postData.description = data["Description"]
+		fmt.Printf("DONE\n")
 
-// 	postData.date, err = time.Parse("2006-01-02", data["Date"])
-// 	if err != nil {
-// 		return nil, fmt.Errorf("could not parse date %q: %w", data["Date"], err)
-// 	}
+		return nil
+	})
+}
 
-// 	postData.template, err = b.templates.FromFile(data["Template"])
-// 	if err != nil {
-// 		return nil, fmt.Errorf("could not get template %q: %w", data["Template"], err)
-// 	}
+func (b *builderImpl) handleMd(
+	path, outP, defaultTplP string,
+	publicAssets map[string]string,
+	getPongo2Ctx func(map[string]string) (pongo2.Context, error),
+) (pongo2.Context, error) {
+	fb, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not read post %q: %w", path, err)
+	}
 
-// 	return postData, nil
-// }
+	buf := new(bytes.Buffer)
 
-// func (b *builderImpl) handleHTMLFile(path string) error {
-// 	return nil
-// }
+	// Render markdown
+	ctx := parser.NewContext()
+	err = b.markdown.Convert(fb, buf, parser.WithContext(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("could not render markdown in %q: %w", path, err)
+	}
 
-// func (b *builderImpl) writeHTML(path, compiled string) error {
+	// Get front-matter
+	data, err := msi2mss(meta.Get(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("could not process front-matter on %q: %w", path, err)
+	}
 
-// 	// 	outFilepath := filepath.Join(b.config.OutputDir,
-// 	// 		strings.TrimPrefix(path, b.config.PagesDir+string(filepath.Separator)))
-// 	// 	outFilepath = outFilepath[0:len(outFilepath)-2] + "html"
+	// The default template can be overridden with a front-matter
+	// directive
+	var tplP string
+	if dat, ok := data["template"]; ok {
+		tplP = dat
+	} else {
+		tplP = defaultTplP
+	}
 
-// 	// 	err = ioutil.WriteFile(outFilepath, []byte(page), os.FileMode(0644))
-// 	// 	if err != nil {
-// 	// 		return fmt.Errorf("could not write file %q: %w", outFilepath, err)
-// 	// 	}
+	// Get the base template
+	bTpl, err := b.templates.FromFile(tplP)
+	if err != nil {
+		return nil, fmt.Errorf("could not get template %q: %w", tplP, err)
+	}
 
-// 	return nil
-// }
+	// Compile an intermediate template in case there are template directives
+	// inside the markdown file
+	itpl, err := b.templates.FromString(buf.String())
+	if err != nil {
+		return nil, fmt.Errorf("could not compile intermediate template: %w", err)
+	}
+
+	mdS, err := itpl.Execute(pongo2.Context{"assets": publicAssets})
+	if err != nil {
+		return nil, fmt.Errorf("could not render intermediate template: %w", err)
+	}
+
+	p2ctx, err := getPongo2Ctx(data)
+	if err != nil {
+		return nil, fmt.Errorf("could create pongo2 context: %w", err)
+	}
+
+	p2ctx["content"] = mdS
+	p2ctx["assets"] = publicAssets
+
+	// Finally create the output file and write content to it
+	outF, err := os.Create(outP)
+	if err != nil {
+		return nil, fmt.Errorf("could not create file %q: %w", outP, err)
+	}
+	defer outF.Close()
+
+	// We write the output from rendering the base template
+	err = bTpl.ExecuteWriter(p2ctx, outF)
+	if err != nil {
+		return nil, fmt.Errorf("could not render template %q to %q: %w", tplP, outP, err)
+	}
+
+	return p2ctx, nil
+}
+
+func (b *builderImpl) mkOutDir(path string) error {
+	// We replace the first dir in the path with the output dir. We expect
+	// split[0] to be $b.config.PublicDir.
+	split := strings.Split(path, string(os.PathSeparator))
+	split[0] = b.config.OutputDir
+	dirP := filepath.Join(split...)
+
+	err := os.MkdirAll(dirP, os.FileMode(_ReadWriteExecute))
+	if err != nil {
+		return fmt.Errorf("could not create directory %q: %w", dirP, err)
+	}
+
+	return nil
+}
 
 func msi2mss(msi map[string]interface{}) (map[string]string, error) {
-	data := make(map[string]string, 0)
+	data := make(map[string]string)
 
 	for key, val := range msi {
 		s, ok := val.(string)
